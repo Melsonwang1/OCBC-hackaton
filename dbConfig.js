b7c7e9e3c44930b34bea288bc6a814d32c780443
melson wang <88783744+Melsonwang1@users.noreply.github.com>
2024-11-07 00:29:44 +0800

    english start page finish

--- dbConfig.js
+++ dbConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-    user: "testing123", // Replace with your SQL Server login username (testing1234)
-    password: "testing123", // Replace with your SQL Server login password (testingtesting1234)
+    user: "testing1234", // Replace with your SQL Server login username (testing1234)
+    password: "testingtesting1234", // Replace with your SQL Server login password (testingtesting1234)
     server: "localhost",
     database: "fsdpSQL",
     trustServerCertificate: true,
