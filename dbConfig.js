aa71d028137060aee8043242c261ffea29081dae
melson wang <88783744+Melsonwang1@users.noreply.github.com>
2025-02-05 13:21:40 +0800

    s

--- dbConfig.js
+++ dbConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-    user: "fsdp", // Replace with your SQL Server login username (testing1234)
-    password: "fsdp", // Replace with your SQL Server login password (testingtesting1234)
+    user: "testing1234", // Replace with your SQL Server login username (testing1234)
+    password: "testingtesting1234", // Replace with your SQL Server login password (testingtesting1234)
     server: "localhost", // vaish changed this from localhost to localhost\\SQLEXPRESS for a while
     database: "FSDP",
     trustServerCertificate: true,
